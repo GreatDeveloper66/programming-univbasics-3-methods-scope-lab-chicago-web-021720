12c750c2423183c7c40f82311289818799572466
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-11 20:49:19 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,11 @@ status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def toadstool
   puts status
 end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def any_phrase(phrase)
+  puts "Do A Barrel Roll!"
+end
